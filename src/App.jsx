4fec6d989d1e735da59f0107caac72358628e630
acgovardhan <mail2acgovardhan@gmail.com>
2025-09-16 23:19:41 +0530

    (fea): create multiple rooms

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useRef } from "react" 
+import { ToastContainer, toast } from 'react-toastify';
 
 import Auth from "./components/Auth"
 import Chat from "./components/Chat";
@@ -8,9 +9,14 @@ import Cookies from "universal-cookie"
 import { signOut } from "firebase/auth";
 import { auth } from "./firebase-config";
 
+import countFunc from "./helper";
+
 const cookies = new Cookies();
 
 function App() {
+
+  const notify = (msg) => toast.error(msg);
+
 
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
   const [room, setRoom] = useState(null)
@@ -24,12 +30,31 @@ function App() {
     setRoom(null)
   }
 
+  const handleEnterChat = async (roomName) =>{
+    const count = await countFunc(roomName)
+    if(count<=0){
+      notify('Room does not exist!')
+    }
+    else{
+      setRoom(roomName)
+    }
+  }
+
+  const handleCreateRoom = async (roomName) =>{
+    const count = await countFunc(roomName)
+    if(count > 0){
+      notify('Room already exist, Try Another Name!')
+    }
+    else{
+      setRoom(roomName)
+    }
+  }
+
   if(!isAuth){
     return (
       <div className="bg flex">
         <Auth setIsAuth={setIsAuth}/>
         <h1 className="main-text">DESI CHAT</h1>
-        
       </div>
     );
   }
@@ -45,10 +70,12 @@ function App() {
         <div className="flex-col">
           <h1 className="sub-text" style={{color: "#103cc2", marginBottom:"20px"}}>Welcome</h1>
 
+          <input type="text" ref={roomInputRef} placeholder="Enter Room Name" className="inputGroup" style={{marginBottom: "20px"}}/>
           <div>
-            <input type="text" ref={roomInputRef} placeholder="Enter Room Name.. (eg. room1)" className="inputGroup"/>
-            <button className="btn2" onClick={() => setRoom(roomInputRef.current.value)}>Enter Chat</button>
+            <button className="btn2" onClick={() => handleEnterChat(roomInputRef.current.value)} style={{marginRight: '80px'}}>Enter Chat</button>
+            <button className="btn2" onClick={() => handleCreateRoom(roomInputRef.current.value)}>Create Room</button>
           </div>
+          <ToastContainer/>
         </div>
       )} 
 
--- src/helper.js
+++ src/helper.js
@@ -1,9 +1,11 @@
 import { collection, query, where, getCountFromServer } from 'firebase/firestore'
 import { db } from './firebase-config'
 
-export default async (roomName) =>{
+const countFunc =  async (roomName) =>{
   const messageRef = collection(db, "messages")
   const messageQuery = query(messageRef, where("room", "==", roomName));
   const snapshot = await getCountFromServer(messageQuery)
   return snapshot.data().count
 }
+
+export default countFunc
